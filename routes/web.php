5a22162af2088dee3a93d5ff03b3bb8c6baf0598
zegarr <zegarr96@gmail.com>
2022-04-29 00:44:15 -0300

    Cambios minimos

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+use App\Models\Players;
 
 Route::get('/', function () {
     return view('home');
 })->name("home");
 
-Route::get('/jugadores', function () {
-    return view('jugadores');
+Route::get('/jugadores/{lang?}', function ($lang = 'es') {
+    App::setLocale("$lang");
+    $jugadores = Players::all();
+    return view('jugadores', ['jugadores' => $jugadores]);
 })->name("jugadores");
 
 Route::get('/jugadorFutbol/{nombre}', function () {
