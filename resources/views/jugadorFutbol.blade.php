3abf18b7946b57fb9c522a00070b4a93d2cdf19d
zegarr <zegarr96@gmail.com>
2022-05-03 23:44:10 -0300

    Cambios en perfil de jugador

--- resources/views/jugadorFutbol.blade.php
+++ resources/views/jugadorFutbol.blade.php
@@ -1,6 +1,6 @@
 @extends("base.base")
 
-@section('titulo', 'Jugador de fútbol')
+@section('titulo', '')
 @section('navegacion')
 <ol>
   <li><a href="./">Inicio</a></li>
@@ -14,7 +14,7 @@
   <section id="about" class="about">
 
     <!-- ======= About Me ======= -->
-    <div class="about-me container">
+    <div class="about-me container mb-5">
 
       <div class="section-title">
         <h2>Perfil</h2>
@@ -22,37 +22,40 @@
       </div>
 
       <div class="row" style="margin-bottom: 65px;">
-        <div class="col-lg-3" style="position: relative;">
-          <img src="assets/img/players/terans.jpg" style="border-radius: 40px;border-bottom: solid #ef6602 14px;"
+        <div class="col-lg-3 col-md-6" style="position: relative;">
+          <div class="row">
+            <div class="col-12" style="justify-content: center;display: flex;">
+              <img src="assets/img/players/terans.jpg" style="border-radius: 40px;border-bottom: solid #ef6602 14px;width: 90%;"
             class="img-fluid" alt="">
-          <a href="https://www.transfermarkt.es/david-terans/profil/spieler/261309" class="btn btn-orange" style="border-radius:45px;position: absolute;bottom: -50px;left: 65px; width: 200px;">Transfer Mareket</a>
+            </div>
+            <div class="col-12" style="justify-content: center;display: flex;">
+              <a href="https://www.transfermarkt.es/david-terans/profil/spieler/261309" class="btn btn-orange" style="border-radius:45px;margin-top: 10px;" target="_blank">Transfer Mareket</a>
+            </div>
+          </div>
         </div>
-        <div class="col-lg-9 pt-4 pt-lg-0">
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Al33J0M7dzk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="col-lg-9 col-md-6 pt-lg-0 pt-5">
+          <iframe style="border-radius: 20px;" width="100%" height="380px" src="https://www.youtube.com/embed/Al33J0M7dzk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
-      <div class="row content">
-        <div class="col-12 col-sm-3">
+      <div class="row content" style="box-shadow: rgb(34 25 21 / 68%) 0px 13px 27px -5px, rgb(0 0 0 / 30%) 0px 8px 16px -8px;border-radius: 19px;padding: 26px;">
+        <div class="section-title mb-2">
+          <h2>Más Info.</h2>
+        </div>
+        <div class="col-12 col-md-4">
           <ul>
             <li><i class="bi bi-chevron-right"></i> <strong>Nacimiento:</strong> <span>11/08/1994</span></li>
             <li><i class="bi bi-chevron-right"></i> <strong>Edad:</strong> <span>27</span></li>
-            </li>
-          </ul>
-        </div>
-        <div class="col-12 col-sm-3">
-          <ul>
             <li><i class="bi bi-chevron-right"></i> <strong>Posición:</strong> <span>Delantero</span></li>
-            <li><i class="bi bi-chevron-right"></i> <strong>Equipo Actual:</strong> <span>Atletico Paranaense</span>
-            </li>
           </ul>
         </div>
-        <div class="col-12 col-sm-3">
+        <div class="col-12 col-sm-4">
           <ul>
+            <li><i class="bi bi-chevron-right"></i> <strong>Equipo Actual:</strong> <span>Atletico Paranaense</span></li>
             <li><i class="bi bi-chevron-right"></i> <strong>Estatura:</strong> <span>1.71m</span></li>
             <li><i class="bi bi-chevron-right"></i> <strong>Peso:</strong> <span>63kg</span></li>
           </ul>
         </div>
-        <div class="col-12 col-sm-3">
+        <div class="col-12 col-sm-4">
           <ul>
             <li><i class="bi bi-chevron-right"></i> <strong>Pierna Hábil:</strong> <span>Izquierda</span></li>
             <li><i class="bi bi-chevron-right"></i> <strong>Nacionalidad:</strong> <span>Uruguayo</span></li>
@@ -69,78 +72,35 @@
         <h2>Galeria</h2>
       </div>
 
-      <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+      <div class="testimonials-slider swiper">
         <div class="swiper-wrapper">
 
           <div class="swiper-slide">
-            <div class="testimonial-item">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium
-                quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-              <h3>Saul Goodman</h3>
-              <h4>Ceo &amp; Founder</h4>
+            <div class="testimonial-item" style="padding: 30px;border-radius: 25px;">
+              <img style="border-radius: 25px;" src="assets/img/players/terans2.jpg" class="img-fluid">
             </div>
-          </div><!-- End testimonial item -->
-
+          </div>
+          
           <div class="swiper-slide">
-            <div class="testimonial-item">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis
-                quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-              <h3>Sara Wilsson</h3>
-              <h4>Designer</h4>
+            <div class="testimonial-item" style="padding: 30px;border-radius: 25px;">
+              <img style="border-radius: 25px;" src="assets/img/players/terans2.jpg" class="img-fluid">
             </div>
-          </div><!-- End testimonial item -->
-
+          </div>
           <div class="swiper-slide">
-            <div class="testimonial-item">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor
-                labore quem eram duis noster aute amet eram fore quis sint minim.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-              <h3>Jena Karlis</h3>
-              <h4>Store Owner</h4>
+            <div class="testimonial-item" style="padding: 30px;border-radius: 25px;">
+              <img style="border-radius: 25px;" src="assets/img/players/terans2.jpg" class="img-fluid">
             </div>
-          </div><!-- End testimonial item -->
-
+          </div>
           <div class="swiper-slide">
-            <div class="testimonial-item">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim
-                dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-              <h3>Matt Brandon</h3>
-              <h4>Freelancer</h4>
+            <div class="testimonial-item" style="padding: 30px;border-radius: 25px;">
+              <img style="border-radius: 25px;" src="assets/img/players/terans2.jpg" class="img-fluid">
             </div>
-          </div><!-- End testimonial item -->
-
+          </div>
           <div class="swiper-slide">
-            <div class="testimonial-item">
-              <p>
-                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa
-                labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-              </p>
-              <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-              <h3>John Larson</h3>
-              <h4>Entrepreneur</h4>
+            <div class="testimonial-item" style="padding: 30px;border-radius: 25px;">
+              <img style="border-radius: 25px;" src="assets/img/players/terans2.jpg" class="img-fluid">
             </div>
-          </div><!-- End testimonial item -->
+          </div>
 
         </div>
         <div class="swiper-pagination"></div>
