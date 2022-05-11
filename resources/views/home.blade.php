45036ad5b1752f833dc489445fd6f9b3b7f6b624
zegarr <zegarr96@gmail.com>
2022-05-10 22:31:31 -0300

    Cambios minimos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <section id="hero" style="height: 100vh;" class="d-flex flex-column justify-content-center align-items-center">
         <h2 class="animate__animated animate__fadeInDown" style="text-align: center"><a style="color:#ff6a00;"
                 href="javascript:void(0);">#</a><span class="tituloPrincipal"></span></h2>
-        <div id="footer" style="    background-color: transparent;position: absolute;bottom: 0px;padding: 0px;">
+        <div id="footer" style="background-color: transparent;position: absolute;bottom: 0px;padding: 0px;">
             <div class="container">
                 <div class="social-links">
                     <a href="https://twitter.com/squadramd" class="twitter"><i class="bx bxl-twitter"></i></a>
