d7318263c9ed2042fd66a4f1248702172c0a3772
zegarr <zegarr96@gmail.com>
2022-07-04 00:02:26 -0300

    agrego videos en contacto y actualizo a heredia

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -16,14 +16,14 @@
                     <div class="col-12 mb-10">
                         <p class="mb-5">{{ __('texto_quienes_somos') }}</p>
                     </div>
-                    <div class="col-6 text-center">
-                        <p class="mb-5">Video 1</p>
+                    <div class="col-12 col-md-6 text-center mb-5">
+                        <video width="100%" src="{{asset("assets/video/mar_vid.mp4")}}" controls></video>
                     </div>
-                    <div class="col-6 text-center">
-                        <p class="mb-5">Video 2</p>
+                    <div class="col-12 col-md-6 text-center mb-5">
+                        <video width="100%" src="{{asset("assets/video/squadra.mp4")}}" controls></video>
                     </div>
-                    <div class="col-sm-4 offset-sm-4">
-                        <h1 class="text-center">Contacto</h1>
+                    <div class="col-sm-8 offset-sm-2 col-xl-4 offset-xl-4">
+                        <h1 class="text-center">{{__("Contacto")}}</h1>
                         <h1 class="text-center"
                             style="font-size:30px;font-family:'Open Sans', sans-serif;background-color: #fc5725;border-radius: 61px;padding: 6px 21px 6px 21px;">
                             <a href="mailto:info@squadramd.com" style="color: white;">info@squadramd.com</a>
